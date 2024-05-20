599e8dbc11dbc1b4ec725a56df76a1ddb44318ab
mcMineyC <jedi.master@falconhosting.net>
2024-05-20 13:26:56 -0700

    Change things

---
+++ compile_lexicon.py
@@ -0,0 +1,14 @@
+import json, compiler
+
+lexicon_file = open("lexicon.txt", 'r')
+lexicon_str = lexicon_file.read()
+lexicon_file.close()
+
+compiler.format_lexicon(input=lexicon_str, output_file="lexicon.json")
+f_in = open("lexicon.json", 'r')
+lexicon = json.load(f_in)
+f_in.close()
+lexicon_list = compiler.compile_to_list(lexicon)
+f_out = open("lexicon_list.json", 'w')
+json.dump(lexicon_list, f_out, indent=4)
+f_out.close()
---
+++ compiler.py
@@ -0,0 +1,48 @@
+import json, hashlib
+from regexes import grid_paragraph_regex, txt_to_RLE, RLE_regex, comment_to_dict
+
+def hasher(MD5Input):
+    return hashlib.md5(MD5Input.encode()).hexdigest()
+
+def format_lexicon(input, output_file):
+    #store contents of lexicon.txt to lexicon_array.json with proper formatting
+    #search lexicon
+    lexicon_paragraphs = grid_paragraph_regex.findall(input)
+
+    #convert to desired Python format
+    json_master_library = {}
+    for paragraph in lexicon_paragraphs:
+         formatted = convert_lexicon(paragraph)
+         json_master_library[formatted["hash"]] = formatted
+    
+    #write to lexicon_array.json
+    json_str = json.dumps(json_master_library, indent=4)
+    json_file = open(output_file, 'w')
+    json_file.write(json_str)
+    json_file.close()
+
+def convert_lexicon(paragraph):
+        rle = txt_to_RLE(paragraph[2])
+        raw_rle = rle.split("\n")[2]
+        name = paragraph[0]
+        hash = hasher(name + rle)
+        creator = "LifeWiki Lexicon"
+        xy = RLE_regex.findall(rle)
+        x = xy[0][2]
+        y = xy[0][3]
+        comments = comment_to_dict(xy[0][1])
+        return {
+            "hash": hash,
+            "name": name,
+            "creator": creator,
+            "xbounds": x,
+            "ybounds": y,
+            "rle": raw_rle,
+            "raw_rle": rle,
+            "comments": comments,
+       }
+
+def compile_to_list(input_dict):
+    #converts the lexicon from a dictionary to a list
+    #this is for the frontend to use
+    return [input_dict[key] for key in input_dict]
