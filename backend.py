f78c93136779a3d4ba9d9c76e07b67646ec529b3
mcMineyC <jedi.master@falconhosting.net>
2024-05-21 18:25:47 -0700

    chore: Refactor backend to improve modularity and add API endpoints for pattern retrieval

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import json, typesense
+import regexes
 
 client = typesense.Client({
     'nodes': [{
@@ -32,6 +33,29 @@ def get_patterns():
         'patterns': pats
     })
 
+@app.route('/patterns/get-named')
+@cross_origin()
+def get_pattern_by_id():
+    print("Sending pattern by ID")
+    pats = json.load(open('patterns.json'))
+    try:
+        return json.dumps({"success": True, "result": pats[request.args.get('id')]})
+    except:
+        print("Error: Pattern not found")
+        return json.dumps({"success": False, "error": "Pattern not found"})
+
+
+@app.route('/lexicon/get-named')
+@cross_origin()
+def get_lexicon_by_id():
+    print("Sending pattern from lexicon by ID")
+    pats = json.load(open('lexicon.json'))
+    try:
+        return json.dumps({"success": True, "result": pats[request.args.get('id')]})
+    except:
+        print("Error: Pattern not found")
+        return json.dumps({"success": False, "error": "Pattern not found in lexicon"})
+
 @app.route('/lexicon/get')
 @cross_origin()
 def get_lexicon():
@@ -58,7 +82,14 @@ def search_lexicon():
         'patterns': list
     }, indent=4)
 
-
+@app.route('/translate', methods=['POST'])
+@cross_origin()
+def translate():
+    print("Translating pattern")
+    return json.dumps({
+        'success': True,
+        'result': {}
+    }, indent=4)
 
 
 if __name__ == '__main__':
