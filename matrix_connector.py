09b643244028f96216daa468c3e1afc669be3a10
mcMineyC <jedi.master@falconhosting.net>
2024-06-02 19:13:37 -0700

    Update connector

--- matrix_connector.py
+++ matrix_connector.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python
-
-# (This is an example similar to an example from the Adafruit fork
-#  to show the similarities. Most important difference currently is, that
-#  this library wants RGB mode.)
-#
-# A more complex RGBMatrix example works with the Python Imaging Library,
-# demonstrating a few graphics primitives and image loading.
-# Note that PIL graphics do not have an immediate effect on the display --
-# image is drawn into a separate buffer, which is then copied to the matrix
-# using the SetImage() function (see examples below).
-# Requires rgbmatrix.so present in the same directory.
-
-# PIL Image module (create or load images) is explained here:
-# http://effbot.org/imagingbook/image.htm
-# PIL ImageDraw module (draw shapes to images) explained here:
-# http://effbot.org/imagingbook/imagedraw.htm
-
 from PIL import Image
 from PIL import ImageDraw
 import io
