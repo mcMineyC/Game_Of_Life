30df2cd614b2cc1a0af73f0a0bb796416a093791
mcMineyC <jedi.master@falconhosting.net>
2024-06-03 10:26:34 -0700

    quick testing

--- connector_test.py
+++ connector_test.py
@@ -11,13 +11,21 @@ client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 # Connect to the server
 client.connect(socket_path)
 
+"""
 # Send a message to the server
 i = matrix_to_image.pro_print_grid_image(CGOL_test_patterns.master_library["snark loop"], (0, 0))
 message = io.BytesIO()
 i.save(message, "PNG")
 message = message.getvalue()
 client.sendall(message)
-
+"""
+o = ""
+for x in range(64):
+    for y in range(64):
+        o += "1"
+    o += "\n"
+client.sendall(o.encode())
+print("sent")
 # Receive a response from the server
 response = client.recv(1024)
 client.close()
